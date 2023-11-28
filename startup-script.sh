3dd500d3087f2a6297c83bc8f445e64312a402f3
lukicsl <lukic@gmx.ch>
2023-11-28 18:11:18 +0100

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -16,8 +16,8 @@ if [ -z "$WATCHDOG_FILE" ] || [ -z "$WATCHDOG_CHANGE" ]; then
 fi
 
 # Ändern der /etc/watchdog.conf mit den Umgebungsvariablen
-sed -i "s|#file=.*|file=$WATCHDOG_FILE|" /etc/watchdog.conf
-sed -i "s|#change=.*|change=$WATCHDOG_CHANGE|" /etc/watchdog.conf
+sed -i "s|#file=.*|file=$WATCHDOG_FILE|" /host_etc/watchdog.conf
+sed -i "s|#change=.*|change=$WATCHDOG_CHANGE|" /host_etc/watchdog.conf
 
 # Neustart des Watchdog-Dienstes
 nsenter --target 1 --mount --uts --ipc --net --pid -- service watchdog restart
