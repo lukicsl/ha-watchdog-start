724d4a2769a8eaae6dce200ed50611fa5d509d69
lukicsl <lukic@gmx.ch>
2023-11-29 14:31:35 +0100

    Update cleanup-script.sh

--- cleanup-script.sh
+++ cleanup-script.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# Überprüfen, ob die erforderlichen Umgebungsvariablen gesetzt sind
+if [ -z "$HOST_NAME" ]  || [ -z "$USER_NAME" ] ; then
+    echo "Die Umgebungsvariablen USER_NAME HOST_NAME müssen gesetzt sein."
+    exit 1
+fi
+
+# Define the host and user
+HOST=$HOST_NAME 
+USER=$USER_NAME
+
 # Setzen Sie die ursprüngliche /etc/watchdog.conf zurück
 cp /etc/watchdog.conf.default /etc/watchdog.conf
 
-# Neustart des Watchdog-Dienstes auf dem Host
-nsenter --target 1 --mount --uts --ipc --net --pid -- service watchdog restart
+# Command to restart watchdog service over SSH
+SSH_CMD="ssh -o StrictHostKeyChecking=no -i /root/.ssh/id_rsa $USER@$HOST systemctl restart watchdog"
+
+# Execute the command
+echo "Restarting watchdog service on the host..."
+$SSH_CMD
