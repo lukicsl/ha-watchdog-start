bfad2c3f307e262ce005397aba8dd4c0926295fe
lukicsl <lukic@gmx.ch>
2023-11-29 14:30:22 +0100

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Überprüfen, ob die erforderlichen Umgebungsvariablen gesetzt sind
+if [ -z "$WATCHDOG_FILE" ] || [ -z "$WATCHDOG_CHANGE" ]  || [ -z "$HOST_NAME" ]  || [ -z "$USER_NAME" ] ; then
+    echo "Die Umgebungsvariablen WATCHDOG_FILE WATCHDOG_CHANGE USER_NAME HOST_NAME müssen gesetzt sein."
+    exit 1
+fi
+
+# Define the host and user
+HOST=$HOST_NAME 
+USER=$USER_NAME
+
 # Definiert die Cleanup-Funktion
 cleanup() {
     echo "SIGTERM-Signal empfangen, rufe cleanup-script.sh auf..."
@@ -9,18 +19,18 @@ cleanup() {
 # SIGTERM-Falle einrichten
 trap cleanup SIGTERM
 
-# Überprüfen, ob die erforderlichen Umgebungsvariablen gesetzt sind
-if [ -z "$WATCHDOG_FILE" ] || [ -z "$WATCHDOG_CHANGE" ]; then
-    echo "Die Umgebungsvariablen WATCHDOG_FILE und WATCHDOG_CHANGE müssen gesetzt sein."
-    exit 1
-fi
 
 # Ändern der /etc/watchdog.conf mit den Umgebungsvariablen
 sed -i "s|#file=.*|file=$WATCHDOG_FILE|" /host_etc/watchdog.conf
 sed -i "s|#change=.*|change=$WATCHDOG_CHANGE|" /host_etc/watchdog.conf
 
-# Neustart des Watchdog-Dienstes
-nsenter --target 1 --mount --uts --ipc --net --pid -- service watchdog restart
+# Command to restart watchdog service over SSH
+SSH_CMD="ssh -o StrictHostKeyChecking=no -i /root/.ssh/id_rsa $USER@$HOST systemctl restart watchdog"
 
-# Halten Sie den Container am Laufen
+# Execute the command
+echo "Restarting watchdog service on the host..."
+$SSH_CMD
+
+# Keep the container running
 tail -f /dev/null
+
