76f6cb52310dd30a4fb97a45d674fd63bd05c751
lukicsl <lukic@gmx.ch>
2023-11-29 15:40:30 +0100

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -21,8 +21,8 @@ trap cleanup SIGTERM
 
 
 # Ändern der /etc/watchdog.conf mit den Umgebungsvariablen
-sed -i "s|#file=.*|file=$WATCHDOG_FILE|" /host_etc/watchdog.conf
-sed -i "s|#change=.*|change=$WATCHDOG_CHANGE|" /host_etc/watchdog.conf
+sed -i "s|#file\s*=\s*.*|file=$WATCHDOG_FILE|" /host_etc/watchdog.conf
+sed -i "s|#change\s*=\s*.*|change=$WATCHDOG_CHANGE|" /host_etc/watchdog.conf
 
 # Command to stop the watchdog service over SSH
 STOP_CMD="ssh -o StrictHostKeyChecking=no -i /root/.ssh/id_rsa $USER@$HOST sudo systemctl stop watchdog"
